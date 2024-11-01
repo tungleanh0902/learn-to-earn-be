eb0b3dd12f46d55b0e7ccbd3bf1002b048ea4e85
tungla3 <tungla@eupsolution.net>
2024-11-01 15:14:52 +0700

    fix: git

--- src/config.ts
+++ src/config.ts
@@ -4,4 +4,5 @@ export const config = {
     'secret': 'nodeauthsecret',
     // 'database': `mongodb+srv://${process.env.NEXT_PUBLIC_MONGO_NAME}:${process.env.NEXT_PUBLIC_MONGO_PW}@${process.env.NEXT_PUBLIC_MONGO_CLUSTER}/asset_management?retryWrites=true&w=majority`
     'database': `${process.env.NEXT_PUBLIC_MONGO}/l2e?retryWrites=true&w=majority&authSource=admin` 
+    // update git
 };
