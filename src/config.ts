8c2e29de4d1157aa39de324856d8d10ca2cc5deb
tunglaeup <tungla@eupsolution.net>
2024-11-01 14:34:53 +0700

    fix: connect db

--- src/config.ts
+++ src/config.ts
@@ -2,6 +2,6 @@ require('dotenv').config();
 
 export const config = {
     'secret': 'nodeauthsecret',
-    'database': `mongodb+srv://${process.env.NEXT_PUBLIC_MONGO_NAME}:${process.env.NEXT_PUBLIC_MONGO_PW}@${process.env.NEXT_PUBLIC_MONGO_CLUSTER}/asset_management?retryWrites=true&w=majority`
-    // remove +srv
+    // 'database': `mongodb+srv://${process.env.NEXT_PUBLIC_MONGO_NAME}:${process.env.NEXT_PUBLIC_MONGO_PW}@${process.env.NEXT_PUBLIC_MONGO_CLUSTER}/asset_management?retryWrites=true&w=majority`
+    'database': `${process.env.NEXT_PUBLIC_MONGO}/l2e?retryWrites=true&w=majority&authSource=admin` 
 };
