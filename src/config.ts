d12bd549e48f11245608a05c5a4fcb0ff87a6dd8
Tung Le Anh <tungleanh@MacBook-Pro-cua-Tung.local>
2024-11-01 14:12:03 +0700

    feat: verify ton proof

--- src/config.ts
+++ src/config.ts
@@ -2,5 +2,6 @@ require('dotenv').config();
 
 export const config = {
     'secret': 'nodeauthsecret',
-    'database': `mongodb+srv://${process.env.NEXT_PUBLIC_MONGO_NAME}:${process.env.NEXT_PUBLIC_MONGO_PW}@cluster0.uwsvq.mongodb.net/asset_management?retryWrites=true&w=majority`
+    'database': `mongodb+srv://${process.env.NEXT_PUBLIC_MONGO_NAME}:${process.env.NEXT_PUBLIC_MONGO_PW}@${process.env.NEXT_PUBLIC_MONGO_CLUSTER}/asset_management?retryWrites=true&w=majority`
+    // remove +srv
 };
