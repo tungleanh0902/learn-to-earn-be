e3e023a19f42f8081be2c3866f9540071caa9570
tungla3 <tungla@eupsolution.net>
2024-11-04 15:27:15 +0700

    feat: change mint nft fee

--- src/config.ts
+++ src/config.ts
@@ -4,7 +4,6 @@ require('dotenv').config();
 export const config = {
     'secret': 'nodeauthsecret',
     'database': `${process.env.NEXT_PUBLIC_MONGO}/l2e?retryWrites=true&w=majority&authSource=admin` 
-    // update git
 };
 
 export const tonQuery = axios.create({
@@ -15,5 +14,5 @@ export const MINT_NFT_OPCODE = "0x00000001"
 export const OWNER_ADDRESS = "0QAN0ngNyGR2fjacIEZi3ex-4_AvqnhJ7q9gJ9pnbt7MCUHm"
 export const SAVE_STREAK_FEE = 100000000 // 0.1 TON
 export const MORE_QUIZZ_FEE = 200000000 // 0.2 TON
-export const MINT_NFT_FEE = 10000000 // 1 TON
+export const MINT_NFT_FEE = 20000000 // 2 TON
 export const SHARE_REF = 10 // 10%
