cd96fb8da1f62bceb4884e316af7b66fc3a18c12
tungla3 <tungla@eupsolution.net>
2024-11-04 15:26:11 +0700

    fix: mongo url

--- src/config.ts
+++ src/config.ts
@@ -3,8 +3,7 @@ require('dotenv').config();
 
 export const config = {
     'secret': 'nodeauthsecret',
-    'database': `mongodb+srv://${process.env.NEXT_PUBLIC_MONGO_NAME}:${process.env.NEXT_PUBLIC_MONGO_PW}@${process.env.NEXT_PUBLIC_MONGO_CLUSTER}/asset_management?retryWrites=true&w=majority`
-    // 'database': `${process.env.NEXT_PUBLIC_MONGO}/l2e?retryWrites=true&w=majority&authSource=admin` 
+    'database': `${process.env.NEXT_PUBLIC_MONGO}/l2e?retryWrites=true&w=majority&authSource=admin` 
     // update git
 };
 
